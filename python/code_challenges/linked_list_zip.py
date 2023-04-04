2275cb6140a4d6787ab2250d0b36d671f06961b4
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2023-04-04 01:46:20 -0500

    started

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,2 +1,22 @@
-def zip_lists(a, b):
-    pass
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def __str__(self):
+        current_node = self.head
+        output = ""
+        while current_node:
+            output += "{ " + str(current_node.data) + " } -> "
+            current_node = current_node.next
+        output += "NULL"
+        return output
