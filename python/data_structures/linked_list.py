794e7d3adc90004df7cceff6207c4276afdc7371
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2023-04-07 01:37:24 -0500

    started linked list insertation

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -3,9 +3,30 @@ class LinkedList:
     Put docstring here
     """
 
+    class Node:
+
+        def __init__(self, value, next_node=None):
+            self.value = value
+            self.next_node = next_node
+
     def __init__(self):
         # initialization here
-        pass
+        self.head = None
+
+    def __str__(self):
+        """
+        Returns a string representation of the linked list.
+        """
+        if not self.head:
+            return "NONE"
+
+        output = ""
+        current = self.head
+        while current:
+            output += f"{{ {current.value} }} -> "
+            current = current.next_node
+        output += "NONE"
+        return output
 
     def some_method(self):
         # method body here
