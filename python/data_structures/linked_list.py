b927585c71e46323248d9087cb04665f1c8f4ca1
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2023-04-21 01:11:56 -0500

    added queue

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next_node = None
+
+
 class LinkedList:
     """
     Put docstring here
@@ -5,11 +11,44 @@ class LinkedList:
 
     def __init__(self):
         # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def add_node(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node is not None:
+                current = current.next_node
+            current.next_node = new_node
+
+    def insert(self, value):
+        new_node = Node(value)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def contains(self, value):
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next_node
+        return False
+
+    def __str__(self):
+        if self.head is None:
+            return "NULL"
+
+        values = []
+        current = self.head
+        while current is not None:
+            values.append(str(current.value))
+            current = current.next_node
+
+        values.append("NULL")
+        return " -> ".join(values)
 
 
 class TargetError:
--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
 class Queue:
     """
     Put docstring here
@@ -5,8 +11,34 @@ class Queue:
 
     def __init__(self):
         # initialization here
-        pass
+        self.front = None
+        self.back = None
+        self.size = 0
 
-    def some_method(self):
+    def enqueue(self, value):
         # method body here
-        pass
+        new_node = Node(value)
+        if self.back is None:
+            self.front = new_node
+        else:
+            self.back.next = new_node
+        self.back = new_node
+        self.size += 1
+
+    def dequeue(self):
+        if self.front is None:
+            raise Exception("Queue is empty")
+        value = self.front.value
+        self.front = self.front.next
+        if self.front is None:
+            self.back = None
+        self.size -= 1
+        return value
+
+    def peek(self):
+        if self.front is None:
+            raise Exception("Queue is empty")
+        return self.front.value
+
+    def is_empty(self):
+        return self.front is None
