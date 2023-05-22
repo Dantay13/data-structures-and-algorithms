51a34be466c4eab45bf0184d1358f3c3212a540d
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2023-05-22 00:13:36 -0500

    completed cc 6 and added additional tests

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -24,6 +24,46 @@ class LinkedList:
             current = current.next
         return False
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def insert_before(self, target_value, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            raise TargetError("Empty list: Cannot insert before.")
+        elif self.head.value == target_value:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                if current.next.value == target_value:
+                    new_node.next = current.next
+                    current.next = new_node
+                    return
+                current = current.next
+            raise TargetError("Target value not found: Cannot insert before.")
+
+    def insert_after(self, target_value, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            raise TargetError("Empty list: Cannot insert after.")
+        current = self.head
+        while current:
+            if current.value == target_value:
+                new_node.next = current.next
+                current.next = new_node
+                return
+            current = current.next
+        raise TargetError("Target value not found: Cannot insert after.")
+
     def __str__(self):
         # method body here
         values = []
@@ -38,4 +78,8 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+
+class TargetError(Exception):
+    pass
 
