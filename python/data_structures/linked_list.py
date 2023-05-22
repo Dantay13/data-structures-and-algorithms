836db54506c43b10c880a948fe2732d6c698dddb
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2023-05-21 20:24:58 -0500

    completed cc 5

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -5,12 +5,38 @@ class LinkedList:
 
     def __init__(self):
         # initialization here
-        pass
+        self.head = None
 
-    def some_method(self):
+    def insert(self, value):
         # method body here
-        pass
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+    def includes(self, value):
+        # method body here
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+    def __str__(self):
+        # method body here
+        values = []
+        current = self.head
+        while current:
+            values.append(str(current.value))
+            current = current.next
+        return ' -> '.join(['{ ' + value + ' }' for value in values] + ['NULL'])
 
 
-class TargetError:
-    pass
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
