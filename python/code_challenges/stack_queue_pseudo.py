497f49f8439520e57d43e17fbb0dd7cc4d9e6d42
DANNER TAYLOR <DANNER_TN@HOTMAIL.COM>
2023-05-22 07:33:19 -0500

    completed cc11

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,20 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        self.stack1 = Stack()
+        self.stack2 = Stack()
+
+    def enqueue(self, value):
+        # Move all elements from stack1 to stack2
+        while not self.stack1.is_empty():
+            self.stack2.push(self.stack1.pop())
+        # Push the new value to stack1
+        self.stack1.push(value)
+        # Move all elements back from stack2 to stack1
+        while not self.stack2.is_empty():
+            self.stack1.push(self.stack2.pop())
+
+    def dequeue(self):
+        # Remove and return the top element from stack1
+        return self.stack1.pop()
